484ca551f9d50ba2e0200eabc19af97732f4e0ac
salvadarroyo <133098621+salvadarroyo@users.noreply.github.com>
2023-05-11 11:38:10 +0200

    Add files via upload

---
+++ Talent JAVA/Clase 12/src/main/java/main.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class main {
+    public static void main(String[] args) throws Exception{
+        Scanner s = new Scanner(System.in);
+        System.out.println("¿Qué pokemon quieres buscar?");
+        String pokemon = s.next();
+        try{
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + pokemon);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.connect();
+            int tiempoDeRespuesta = con.getResponseCode();
+            if(tiempoDeRespuesta != 200){
+                throw new RuntimeException("HttpResponseCode" + tiempoDeRespuesta);
+            }
+            else {
+                StringBuilder informacion = new StringBuilder();
+                Scanner sc = new Scanner(url.openStream());
+                while (sc.hasNext()) {
+                    informacion.append(sc.nextLine());
+                }
+                sc.close();
+                JSONObject jsonObject = new JSONObject(informacion.toString());
+                String nombre = jsonObject.getJSONObject("species").getString("name");
+                String tipo = jsonObject.getJSONArray("types").getJSONObject(0).getJSONObject("type").getString("name");
+                int peso = jsonObject.getInt("weight");
+
+                System.out.println("Nombre: " + nombre);
+                System.out.println("Tipo: " + tipo);
+                System.out.println("Peso: " + peso);
+
+                try{
+                    String ruta = "C:\\Users\\sdarroyo\\Documents\\";
+                    String nombreArchivo = "pokemon.txt";
+                    String ruta_nombreArchivo = ruta + nombreArchivo;
+                    File archivo = new File(ruta_nombreArchivo);
+                    FileWriter fw = new FileWriter(archivo);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write("Nombre: " + nombre + "\n");
+                    bw.write("Tipo: " + tipo + "\n");
+                    bw.write("Peso: " + peso + "\n");
+                    fw.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
